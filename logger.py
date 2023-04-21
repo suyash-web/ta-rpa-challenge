cb05db261916076a22505a58873495a464df6e9a
dev <66136072+suyash-web@users.noreply.github.com>
2023-04-21 20:22:49 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,21 +1,9 @@
-"""
-Basic logging, centralized so sinks/other logging necessities can be customized centrally
-"""
 import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.propagate = False
-
-formatter = logging.Formatter(
-    r"%(asctime)s - %(levelname)-7s %(threadName)-12s [%(filename)s:%(lineno)s - %(funcName)s()] - %(message)s"
-)
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-if __name__ == "__main__":
-    logger.info("Info logging test")
-    logger.warning("Warning logging test")
-    logger.error("Error logging test")
-    logger.exception(Exception("Exception logging test"))
+logger = logging.getLogger("log")
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)

4d1bea99b0f666018731d9d5612a9cf18ccaa56c
dev <66136072+suyash-web@users.noreply.github.com>
2023-04-21 20:21:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import os
 
-if not os.path.exists(os.getcwd()+"/output"):
-    os.mkdir(os.getcwd()+"/output")
-if not os.path.exists(os.getcwd()+"/images"):
-    os.mkdir(os.getcwd()+"/images")
-
-FILEPATH = os.getcwd()+"/output/Fresh News.xlsx"
-IMAGE_PATH = os.getcwd()+"/images"
+class DIRECTORIES:
+    FILEPATH = os.getcwd()+"/output/Fresh News.xlsx"
+    IMAGE_PATH = os.getcwd()+"/images"

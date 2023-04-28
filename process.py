1063a3afac59913ab3c11ea4b0a40414a2687f00
dev <66136072+suyash-web@users.noreply.github.com>
2023-04-28 23:43:50 +0530

    Update process.py

--- process.py
+++ process.py
@@ -1,8 +1,8 @@
 from fresh_news import NyTimes
 from logger import logger
 from config import DIRECTORIES
-import os
 import shutil
+import os
 
 class Process:
     def __init__(self, workitems: dict) -> None:
@@ -24,26 +24,34 @@ class Process:
         ny_times = NyTimes(self.workitems)
         try:
             flag = False
-            logger.info(f"Searching news for query {ny_times.phrase} and section {ny_times.section} for past {ny_times.months} months...")
-            ny_times.search_query()
-            try:
-                ny_times.set_filters()
-            except AssertionError:
-                flag = True
-                logger.info(f"Section {ny_times.section} is not available.")
+            logger.info(f"Searching news for query {ny_times.phrase} and section {ny_times.section} from past {ny_times.months} months...")
+            search_status = ny_times.search_query()
+            if search_status[0]:
+                try:
+                    ny_times.set_filters()
+                except AssertionError:
+                    flag = True
+                    logger.info("Terminating the process.")
+                if not flag:
+                    total_news = ny_times.total_news()
+                    logger.info(f"{total_news} news found.")
+                    logger.info("Loading all the news...")
+                    all_news_loaded = ny_times.load_all_news()
+                    logger.info("Fetching news data...")
+                    news_fetched = ny_times.fetch_data()
+                    if not all_news_loaded:
+                        logger.info(f"The website could not load all the news. Data for {news_fetched} news has been fetched.")
+                    else:
+                        logger.info("All the news has been fetched.")
+                    logger.info("Ending the process...")
+                    shutil.make_archive(DIRECTORIES.IMAGE_ARCHIVES_PATH, 'zip', DIRECTORIES.IMAGE_PATH)
+                    shutil.rmtree(DIRECTORIES.IMAGE_PATH)
+            else:
+                logger.info(search_status[1])
                 logger.info("Terminating the process.")
-            if not flag:
-                total_news = ny_times.total_news()
-                logger.info(f"{total_news} news found.")
-                ny_times.load_all_news()
-                ny_times.fetch_news_data()
-                logger.info("All the news has been fetched.")
-                logger.info("Ending the process.")
-                shutil.make_archive(DIRECTORIES.IMAGE_ARCHIVES_PATH, 'zip', DIRECTORIES.IMAGE_PATH)
-                shutil.rmtree(DIRECTORIES.IMAGE_PATH)
             ny_times.browser.close_browser()
         except Exception as e:
-            ny_times.browser.screenshot(filename=DIRECTORIES.ERROR_SNAPSHOT_PATH)
+            ny_times.browser.screenshot(filename="output/error.png")
             raise e
 
     def start(self) -> None:
